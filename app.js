b8ee10ad8080c0fbb3f9169a5aa930fa778f407e
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-09 21:44:47 +0530

    express introduction

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
 
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
-
-const server = http.createServer((req, res) => {
-  console.log(req);
-  const url = req.url;
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homePage);
-    res.end();
-  } else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    res.write(homeImage);
-    res.end();
-  } else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    res.write(homeLogic);
-    res.end();
-  } else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    res.write('<h1>Page not found</h1>');
-    res.end();
-  }
+app.get('/', (req, res) => {
+  res.send('Home page');
 });
 
-server.listen(3000);
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send(`<h1>resource not found</h1>`);
+});
+
+app.listen(5000, () => {
+  console.log(`server is listening at 5000...`);
+});
+
+/***
+ * app.get
+ *  app.post
+ * app.put
+ * app.delete
+ * app.all
+ * app.use
+ * app.listen */
