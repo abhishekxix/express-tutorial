229046c37f450c9f6921b43084196192a7be5241
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-09 22:02:02 +0530

    express json basics

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const { products } = require('./data');
 
-// setup static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// adding to static resources
-// server side rendering
-// });
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found');
+app.get('/', (req, res) => {
+  // res.json([{ name: 'susan' }, { name: 'john' }]);
+  res.json(products);
 });
 
 app.listen(3000, () => {
-  console.log('server is listening at 3000...');
+  console.log('Server is running at port 3000...');
 });
