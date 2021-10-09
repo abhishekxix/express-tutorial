fdbf8c9ea8e15e560ce79b3f3aca26eeca11c42a
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-09 22:06:49 +0530

    api basics

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const { products } = require('./data');
 
 app.get('/', (req, res) => {
   // res.json([{ name: 'susan' }, { name: 'john' }]);
-  res.json(products);
+  // res.json(products);
+  res.send('<h1>Home page</h1> <a href="/api/products">Products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
 
 app.listen(3000, () => {
