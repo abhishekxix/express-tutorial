64b90da407ac96df23a6caea97c764406553dd99
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-10 12:20:12 +0530

    HTTP methods

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
-const logger = require('./logger');
-const authorize = require('./authorize');
+let { people } = require('./data');
 
-// makes every mentioined route use this middleware
-// if we don't mention a route, it makes every route use it.
-app.use('/api', [logger, authorize]);
-app.use(express.static('./public'));
-//
+// static assets
+app.use(express.static('./methods-public'));
 
-app.get('/', (req, res) => {
-  res.send('Home');
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+// parse json
+app.use(express.json());
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get('/about', (req, res) => {
-  res.send('About');
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide a valid name' });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
-app.get('/api/products', (req, res) => {
-  res.send('Products');
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (name) {
+    res.status(200).send(`Hello ${name}`);
+  } else {
+    res.status(401).send('Please Provide Credentials');
+  }
 });
 
-app.get('/api/items', (req, res) => {
-  res.send('Items');
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  console.log(id, name);
+  res.send('hello world');
 });
 
 app.listen(3000, () => {
